3dddd656580edbe2ac172da7e727c5b8a61c19c2
Hugo <70238110+hugo-maciel@users.noreply.github.com>
2024-05-03 19:06:49 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    basic.showString("SILVIA")
+    basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
