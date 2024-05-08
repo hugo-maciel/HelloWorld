23e5d142de94eeab1a9d77029a0a9ec5fd00f87c
Hugo <70238110+hugo-maciel@users.noreply.github.com>
2024-05-08 19:28:23 -0300

    Brincadeira Bortopack

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
-basic.forever(function () {
-    basic.showString("SILVIA")
+input.onGesture(Gesture.Shake, function () {
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
+    basic.showString("BORTOPACK")
     basic.showIcon(IconNames.Heart)
     basic.pause(1000)
     basic.showLeds(`
